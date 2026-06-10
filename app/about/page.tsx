12ed6ce92a0d89517f12c0efe80b1a1997421e56
tkmaher <tkmaher@umich.edu>
2026-06-09 20:52:48 -0500

    try this

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,15 +13,12 @@ export default function Page() {
                 About
             </div>
             <div className="about">
-                <div className="tab0">ubutube.org is an archive of an archive. </div>
+                <div className="tab0">ubutube.org</div> 
                 <div className="tab1">
-                    It is a wrapper of the film section of the historic avant-garde media archive <a href="https://ubu.com" className="linkout" target="_blank">UbuWeb</a>.
+                    is an extension of the historic avant-garde media archive <a href="https://ubu.com" className="linkout" target="_blank">UbuWeb</a> that supports account creation, bookmarking, and commenting.
                 </div>
                 <div className="tab1">
-                    (ubutube.org is not affiliated with ubu.com in any way, but was created with the utmost gratitude for the important work of the latter.)
-                </div>
-                <div className="tab2">
-                    UbuTube currently supports account creation, film bookmarking, and commenting. Please keep your activities respectful and intentional.
+                    (ubutube.org is not affiliated with ubu.com in any way.)
                 </div>
                 <br/>
                 <div className="tab1">
