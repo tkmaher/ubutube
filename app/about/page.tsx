03db93edc2e8a9c435f5f56788af67ece97783b5
tkmaher <tkmaher@umich.edu>
2026-06-09 22:20:07 -0500

    ubuabout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,10 +15,13 @@ export default function Page() {
             <div className="about">
                 <div className="tab0">ubutube.org</div> 
                 <div className="tab1">
-                    is an extension of the historic avant-garde media archive <a href="https://ubu.com" className="linkout" target="_blank">UbuWeb</a> that supports account creation, bookmarking, and commenting.
+                    is an extension of the historic avant-garde* media archive <a href="https://ubu.com" className="linkout" target="_blank">UbuWeb</a> that supports account creation, bookmarking, and commenting.
                 </div>
                 <div className="tab2">
                     (ubutube.org is not affiliated with ubu.com in any way.)
+                </div>
+                <div className="tab2">
+                    
                 </div>
                 <br/>
                 <div className="tab1">
@@ -39,6 +42,10 @@ export default function Page() {
                 <br/>
                 <div className="tab2">
                     Built by <a href="https://otherseas1.com" className="linkout" target="_blank">otherseas1</a>
+                </div>
+                <br/>
+                <div className="tab1" style={{fontSize: "14px"}}>
+                    *<a href="https://ubu.com/papers/macdonald_avant_intro.html" className="linkout" target="_blank">Introduction to "Avant-Garde Film" (Scott MacDonald, 1993)</a>
                 </div>
 
             </div>
